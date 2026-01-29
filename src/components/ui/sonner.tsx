199f84434cb5d75565680eb4205d9654d0f85457
HyeungJun Kim <rabbit92carrot@gmail.com>
2026-01-29 21:00:07 +0900

    feat: add UI feedback (badge, card, separator, sonner)

---
+++ src/components/ui/sonner.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { Toaster as Sonner, type ToasterProps } from 'sonner';
+
+function Toaster(props: ToasterProps) {
+  return (
+    <Sonner
+      className="toaster group"
+      toastOptions={{
+        classNames: {
+          toast:
+            'group toast group-[.toaster]:bg-white group-[.toaster]:text-gray-900 group-[.toaster]:border-gray-200 group-[.toaster]:shadow-lg',
+          description: 'group-[.toast]:text-gray-500',
+          actionButton: 'group-[.toast]:bg-blue-600 group-[.toast]:text-white',
+          cancelButton: 'group-[.toast]:bg-gray-100 group-[.toast]:text-gray-500',
+        },
+      }}
+      {...props}
+    />
+  );
+}
+
+export { Toaster };
